6caa594d0299c6f5a5141e7cdbadb9d0adb45910
Teodor Toshkov <ttoshkov@asteasolutions.com>
2019-11-04 15:13:54 +0200

    array problems

--- exercises/04 && 05 - Arrays/problems.cpp
+++ exercises/04 && 05 - Arrays/problems.cpp
@@ -2,7 +2,7 @@
 
 void print(const int arr[], unsigned begin, unsigned end)
 {
-    for (begin; begin != end; ++begin)
+    for (; begin != end; ++begin)
     {
         std::cout << arr[begin] << ' ';
     }
@@ -11,7 +11,7 @@ void print(const int arr[], unsigned begin, unsigned end)
 
 void reverse(int arr[], unsigned begin, unsigned end)
 {
-    for (begin, end; begin != end && begin != --end; ++begin)
+    for (; begin != end && begin != --end; ++begin)
     {
         std::swap(arr[begin], arr[end]);
     }
@@ -20,7 +20,7 @@ void reverse(int arr[], unsigned begin, unsigned end)
 unsigned copy(const int input_arr[], unsigned input_begin, unsigned input_end,
               int output_arr[], unsigned output_begin)
 {
-    for (input_begin; input_begin != input_end; ++input_begin)
+    for (; input_begin != input_end; ++input_begin)
     {
         output_arr[output_begin++] = input_arr[input_begin];
     }
@@ -51,7 +51,7 @@ unsigned insert(int arr[], unsigned size, unsigned index, int element)
 
 unsigned find(const int arr[], unsigned begin, unsigned end, int element)
 {
-    for (begin; begin != end; ++begin)
+    for (; begin != end; ++begin)
     {
         if (arr[begin] == element)
         {
@@ -71,35 +71,113 @@ int inner_product(const int input_arr_1[], unsigned begin_1, unsigned end_1,
                   const int input_arr_2[], unsigned begin_2)
 {
     int result{0};
-    for (begin_1; begin_1 != end_1; ++begin_1, ++begin_2)
+    for (; begin_1 != end_1; ++begin_1, ++begin_2)
     {
         result += input_arr_1[begin_1] * input_arr_2[begin_2];
     }
     return result;
 }
 
-unsigned min_index(const int arr[], unsigned begin, unsigned end)
+unsigned partial_sum(const int input_arr[], unsigned input_begin, unsigned input_end,
+                     int output_arr[], unsigned output_begin)
 {
-    unsigned min_index{begin};
-    int minimum{arr[min_index]};
+    int sum{0};
+    for(; input_begin != input_end; ++input_begin)
+    {
+        output_arr[output_begin++] = sum += input_arr[input_begin];
+    }
+    return output_begin;
+}
 
-    for(begin; begin != end; ++begin)
+unsigned adjacent_difference(const int input_arr[], unsigned input_begin, unsigned input_end,
+                             int output_arr[], unsigned output_begin)
+{
+    int last{0};
+    for(; input_begin != input_end; ++input_begin)
+    {
+        int current{input_arr[input_begin]};
+        output_arr[output_begin++] = current - last;
+        last = current;
+    }
+    return output_begin;
+}
+
+unsigned min_element(const int arr[], unsigned begin, unsigned end)
+{
+    unsigned min_element{begin};
+    int minimum{arr[min_element]};
+
+    for(; begin != end; ++begin)
     {
         if (arr[begin] < minimum)
         {
             minimum = arr[begin];
-            min_index = begin;
+            min_element = begin;
         }
     }
 
-    return min_index;
+    return min_element;
 }
 
 void selection_sort(int arr[], unsigned begin, unsigned end)
 {
-    for (begin; begin != end; begin++)
+    for (; begin != end; begin++)
     {
-        std::swap(arr[begin], arr[min_index(arr, begin, end)]);
+        std::swap(arr[begin], arr[min_element(arr, begin, end)]);
+    }
+}
+
+unsigned merge(const int input_1[], unsigned begin_1, unsigned end_1,
+               const int input_2[], unsigned begin_2, unsigned end_2,
+               int output[], unsigned output_begin)
+{
+    while(begin_1 != end_1 && begin_2 != end_2)
+    {
+        if (input_1[begin_1] <= input_2[begin_2])
+        {
+            output[output_begin++] = input_1[begin_1++];
+        }
+        else
+        {
+            output[output_begin++] = input_2[begin_2++];
+        }
+    }
+
+    copy(input_1, begin_1, end_1, output, output_begin);
+    return copy(input_2, begin_2, end_2,
+                output, output_begin);
+}
+
+unsigned upper_bound(const int arr[], unsigned begin, unsigned end, int element)
+{
+    while (begin < end)
+    {
+        unsigned mid{(begin + end) / 2};
+
+        if (element >= arr[mid])
+        {
+            begin = mid + 1;
+        }
+        else
+        {
+            end = mid;
+        }
+    }
+
+    return begin;
+}
+
+bool binary_search(const int arr[], unsigned begin, unsigned end, int element)
+{
+    unsigned greater = upper_bound(arr, begin, end, element);
+    return greater != begin && arr[greater - 1] == element;
+}
+
+void insertion_sort(int arr[], unsigned begin, unsigned end)
+{
+    for(unsigned i = begin; i != end; ++i)
+    {
+        rotate(arr, upper_bound(arr, begin, i, arr[i]), i, i + 1);
     }
 }
 
@@ -160,7 +238,22 @@ int main()
 
     int sequence[]{ 1, 2, 3, 4 };
 
-    std::cout << "min in sequence: " << sequence[min_index(sequence, 0, 4)] << "\n\n";
+    std::cout << "sequence: ";
+    print(sequence, 0, 4);
+
+    partial_sum(sequence, 0, 4,
+                sequence, 0);
+
+    std::cout << "partial sums: ";
+    print(sequence, 0, 4);
+
+    adjacent_difference(sequence, 0, 4,
+                        sequence, 0);
+
+    std::cout << "adjacent difference: ";
+    print(sequence, 0, 4);
+
+    std::cout << "min in sequence: " << sequence[min_element(sequence, 0, 4)] << "\n\n";
 
     const int sort_example_size = 10;
     int sort_example_arr[]{ 1, 3, 4, 6, 2, 9, 0, 8, 7, 5 };
@@ -172,6 +265,41 @@ int main()
 
     std::cout << "sorted arr: ";
     print(sort_example_arr, 0, sort_example_size);
+
+    const int mergable_1_size = 3;
+    const int mergable_2_size = 2;
+    const int merged_size = mergable_1_size + mergable_2_size;
+
+    const int mergable_1[mergable_1_size]{ 1, 3, 5 };
+    const int mergable_2[mergable_2_size]{ 2, 6 };
+    int merged[merged_size]{};
+
+    merge(mergable_1, 0, mergable_1_size,
+          mergable_2, 0, mergable_2_size,
+          merged, 0);
+
+    std::cout << "\nmergable 1: ";
+    print(mergable_1, 0, mergable_1_size);
+    std::cout << "mergable 2: ";
+    print(mergable_2, 0, mergable_2_size);
+    std::cout << "merged: ";
+    print(merged, 0, merged_size);
+
+    std::cout << "first element bigger than 4 -> " << merged[upper_bound(merged, 0, merged_size, 4)] << '\n';
+
+    std::cout << "Has 0: " << binary_search(merged, 0, merged_size, 0) << '\n';
+    std::cout << "Has 3: " << binary_search(merged, 0, merged_size, 3) << "\n\n";
+
+    const int another_sort_example_size = 10;
+    int another_sort_example_arr[another_sort_example_size]{ 1, 3, 4, 6, 2, 9, 0, 8, 7, 5 };
+
+    std::cout << "unsorted arr: ";
+    print(another_sort_example_arr, 0, another_sort_example_size);
+
+    insertion_sort(another_sort_example_arr, 0, another_sort_example_size);
+
+    std::cout << "insertion sorted arr: ";
+    print(another_sort_example_arr, 0, another_sort_example_size);
 
     return 0;
 }
